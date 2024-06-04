2be23e144c2a088b4d3eb89604145ed621a9100c
박경민 <scsoym33@gmail.com>
2024-06-04 16:48:50 +0900

    240604

---
+++ workspace_Java/chap05/src/chap05/DiceArray.java
@@ -0,0 +1,18 @@
+package chap05;
+
+public class DiceArray {
+
+	public static void main(String[] args) {
+		// 주사위 두개 합 경우의수
+		String[] dice = new String[12];
+
+		for (int i = 0; i < 12; i++)
+			for (int j = 1; j <= 6; j++) {
+				for (int k = 1; k <= 6; k++) {
+					dice[i] =i 
+				}
+			}
+
+	}
+
+}
--- workspace_Java/helloworld/src/helloworld/main/Main.java
+++ workspace_Java/helloworld/src/helloworld/main/Main.java
@@ -4,35 +4,32 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
+
+		// 값 입력받기
 		int[] num = new int[3];
-//		int[] num2 = new int[3];
-
 		for (int i = 0; i < 3; i++) {
-			int a = scan.nextInt();
-			num[i] = a;
+			num[i] = scan.nextInt();
 		}
 
-		int max = 0, min = num[0], m1=0;
-
+		// 오름차순으로 정렬
+		int temp = 0;
 		for (int i = 0; i < 3; i++) {
-			if (max < num[i]) {
-				max = num[i];
-				num[i] = 0;
-			}
-			if (min > num[i]) {
-				min = num[i];
-				num[i] = 0;
+			for (int j = i+1; j < 3; j++) {
+				if (num[j] < num[i]) {
+					temp = num[j];
+					num[j] = num[i];
+					num[i] = temp;
+
+				}
 			}
 		}
 
-		for (int i = 0; i < 3; i++) {
-			if (num[i] != 0) {
-				m1 = num[i];
-			}
-		}
-		
-		System.out.println(m1);
+		// 출력
+		System.out.print(num[0] + " ");
+		System.out.print(num[1] + " ");
+		System.out.println(num[2]);
 
 	}
 }
