ac0a4b241ab7c0650be998fe1e9aa62ce7b0d84e
박경민 <scsoym33@gmail.com>
2024-06-04 16:56:57 +0900

    240604

--- workspace_Java/chap05/src/chap05/DiceArray.java
+++ workspace_Java/chap05/src/chap05/DiceArray.java
@@ -4,14 +4,18 @@ public class DiceArray {
 
 	public static void main(String[] args) {
 		// 주사위 두개 합 경우의수
-		String[] dice = new String[12];
-
-		for (int i = 0; i < 12; i++)
-			for (int j = 1; j <= 6; j++) {
-				for (int k = 1; k <= 6; k++) {
-					dice[i] =i 
-				}
+		String[] dice1 = new String[12];
+		
+		
+		for(int i = 1; i<=6 ; i++) {
+			for(int j=1;j<=6 ; j++) {
+				dice1[i+j-2] = "["+i+", "+j+"]";
 			}
+		}
+		
+		for(int i=0;i<12;i++) {
+			System.out.print(dice1[i]);
+		}
 
 	}
 
