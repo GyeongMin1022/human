34be264ced6283e86b1a9f8279dd110c597bb67d
박경민 <scsoym33@gmail.com>
2024-06-07 17:11:05 +0900

    240607

--- workspace_Java/helloworld/src/helloworld/main/Main.java
+++ workspace_Java/helloworld/src/helloworld/main/Main.java
@@ -2,9 +2,30 @@ package helloworld.main;
 
 import java.util.Scanner;
 
+class C {
+	private int a;
+
+	public void set(int a) {
+		this.a = a;
+	}
+
+	public void add(int d) {
+		a += d;
+	}
+
+	public void print() {
+		System.out.println(a);
+	}
+}
+
 public class Main {
 	public static void main(String[] args) {
-		
+		C p = new C();
+		C q;
+		p.set(10);
+		q = p;
+		p.add(30);
+		p.print();
 
 	}
 }
