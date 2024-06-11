761e2544e17a3c6f69dd3d4907c59697b6961e64
박경민 <scsoym33@gmail.com>
2024-06-11 19:42:08 +0900

    240611

--- workspace_Java/helloworld/src/helloworld/main/InsertionSort.java
+++ workspace_Java/helloworld/src/helloworld/main/InsertionSort.java
@@ -1,26 +1,33 @@
 package helloworld.main;
 
-public class InsertSort {
+public class InsertionSort {
 
 	public static void main(String[] args) {
 		int arr[] = { 5, 2, 1, 4, 3, 7, 6 };
-		int n = arr.length;
+		
 
 		// 정렬
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			int insert = arr[i];
 			int j = i - 1;
 
-			// 숫자를 정렬된 부분에 삽입하기 위해 이동
+			// 숫자를 정렬된 부분에 삽입
 			while (j >= 0 && arr[j] > insert) {
 				arr[j + 1] = arr[j];
 				j = j - 1;
 			}
 			arr[j + 1] = insert;
+			
+            System.out.print(i  + "회전 결과: ");
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k] + " ");
+            }
+            System.out.println();
 		}
 
+		// 출력
 		System.out.print("삽입 정렬 결과: "); // 1 2 3 4 5 6 7
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 	}
--- workspace_Java/helloworld/src/helloworld/main/SelectionSort.java
+++ workspace_Java/helloworld/src/helloworld/main/SelectionSort.java
@@ -1,27 +1,33 @@
 package helloworld.main;
 
-public class SelectSort {
+public class SelectionSort {
 
 	public static void main(String[] args) {
 		 int arr[] = { 5, 2, 1, 4, 3, 7, 6 };
-	        int n = arr.length;
 	        
 	        // 정렬
-	        for (int i = 0; i < n - 1; i++) {
+	        for (int i = 0; i < arr.length - 1; i++) {
 	            int min = i;
-	            for (int j = i + 1; j < n; j++) {
+	            for (int j = i + 1; j < arr.length; j++) {
 	                if (arr[j] < arr[min]) {
 	                    min = j;
 	                }
 	            }
-	            // 최솟값위치와 현재위치를 교환
+	            // 최솟값과 현재 위치를 교환
 	            int temp = arr[min];
 	            arr[min] = arr[i];
 	            arr[i] = temp;
+	            
+	            System.out.print(i + 1 + "회전 결과 : ");
+	            for (int k = 0; k < arr.length; k++) {
+	                System.out.print(arr[k] + " ");
+	            }
+	            System.out.println();
 	        }
-
+	        
+	        // 출력
 	        System.out.print("선택 정렬 결과: "); // 1 2 3 4 5 6 7
-	        for (int i = 0; i < n; i++) {
+	        for (int i = 0; i < arr.length; i++) {
 	            System.out.print(arr[i] + " ");
 	        }
 	}
