7825d991538e951b3a6b75241f9e07874e71af53
박경민 <scsoym33@gmail.com>
2024-07-20 10:48:30 +0900

    240720

---
+++ workspace_python/var.py
@@ -0,0 +1,23 @@
+a = 10
+naver = 1
+naver  = 2 # 변수에 오타 주의
+
+b = a // 3 # 내림
+print(b)
+print(-a // 3)
+
+print(a % 3)
+print(2**a)
+
+a= int(-3.3)
+print(a)
+print(int('100')+1)
+
+# print = 10
+# print(a)
+
+a= 10
+print(type(a))
+a='sadrf'
+print(type(a))
+print(type(print))
