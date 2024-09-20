b5e4322b809ea9e609eebaf9273a0a3115313ebf
박경민 <scsoym33@gmail.com>
2024-09-20 12:41:58 +0900

    240920

---
+++ workspace_cs/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Hello, World!,  ");  /* print */
+        Console.WriteLine("Hello, C#"); /* println */
+
+        int[] products = new int[3];
+
+        int[] p = new int[3];  
+        p[0] = 10;
+        test();
+        int[] arr = {1,2, 3,4,5};
+        foreach(int i in arr)
+        {
+            Console.Write(i + ","); 
+        }
+    }
+    static void test()
+    {
+        Console.WriteLine("test");
+    }
+}
+
+
